1aceac24a0344ccea23f8811ead536e67ee532a7
Sara Johnsson <sara86persson@hotmail.com>
2024-09-19 08:24:08 +0200

    started file system, landing page and header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import './App.css'
+import LandingPage from './views/landingpage/LandingPage'
+import HeaderComponent from './components/headercomponent/HeaderComponent'
 
 function App() {
 
 
   return (
-    <>
+    <body>
+      <HeaderComponent />
+      <Router>
+        <Routes>
 
-    </>
+          <Route path='/' element={<LandingPage />} />
+        </Routes>
+      </Router>
+    </body>
   )
 }
 
---
+++ src/views/landingpage/LandingPage.tsx
@@ -0,0 +1,15 @@
+
+
+
+const LandingPage: React.FC = () => {
+
+  return (
+    <>
+      <section className="landingpage-container">
+        <h1>Welcome to PageTurner</h1>
+      </section>
+    </>
+  )
+}
+
+export default LandingPage;
