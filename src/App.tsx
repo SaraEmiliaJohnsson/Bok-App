14b6df198f8dd88a224cab51f8fa22454266806d
Sara Johnsson <sara86persson@hotmail.com>
2024-09-26 08:36:58 +0200

    adding view for bookshelf

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import './App.css'
 import LandingPage from './views/landingpage/LandingPage'
 import HeaderComponent from './components/headercomponent/HeaderComponent'
+import BookShelfPage from './views/bookshelf/BookShelfPage'
 
 function App() {
 
@@ -12,8 +13,8 @@ function App() {
       <HeaderComponent />
       <Router>
         <Routes>
-
           <Route path='/' element={<LandingPage />} />
+          <Route path='/bookshelf' element={<BookShelfPage />} />
         </Routes>
       </Router>
     </body>
